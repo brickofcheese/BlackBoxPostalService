a49c36ae89026b545c2afd59b62b0865a5cdd418
Felix Adams <felixtadams1@gmail.com>
2017-11-10 09:44:54 -0500

    Updated main OSV code Nov 7

--- OSV_Main_Code/OSV_Main_Code.ino
+++ OSV_Main_Code/OSV_Main_Code.ino
@@ -1,28 +1,36 @@
 #include "Enes100.h"
-Enes100 enes("Black Box Postal Service", BLACK_BOX, 178, 8, 9); //Make sure to change marker number and rx and tx pins to reflect OSV
+#include "IRremote.h"
 
 //Set pin values
-#define infraredLeftPin 0
-#define infraredRightPin 0
-#define crashLeftPin 3
-#define crashMiddlePin 2
-#define crashRightPin 1
+#define infraredLeftPin 11
+#define infraredRightPin 12
+#define crashLeftPin 2
+#define crashMiddlePin 3
+#define crashRightPin 4
 
-#define motorLeftEnable 5
-#define motorLeftOutput1 6
-#define motorLeftOutput2 7
+#define motorLeftEnable A5
+#define motorLeftInput1 8
+#define motorLeftInput2 9
 
-#define motorRightEnable 10
-#define motorRightOutput1 8
-#define motorRightOutput2 9
+#define motorRightEnable A4
+#define motorRightInput1 6
+#define motorRightInput2 7
 
 //Other constants
 #define checkTime 100
 
 //Variable defintitions
-int phase 1;
+int phase = 1;
 int obstacleX;
 int obstacleY;
+int obstacleTheta;
+
+Enes100 enes("Black Box Postal Service", BLACK_BOX, 199, 1, 0); //Make sure to change marker number and rx and tx pins to reflect OSV
+IRrecv irLeft(infraredLeftPin);
+IRrecv irRight(infraredRightPin);
+
+decode_results resultsLeft;
+decode_results resultsRight;
 
 void setup() {
   if(!enes.updateLocation()) {
@@ -30,20 +38,23 @@ void setup() {
   }
   
   pinMode(motorLeftEnable, OUTPUT);
-  pinMode(motorLeftOutput1, OUTPUT);
-  pinMode(motorLeftOutput2, OUTPUT);
+  pinMode(motorLeftInput1, OUTPUT);
+  pinMode(motorLeftInput2, OUTPUT);
 
   pinMode(motorRightEnable, OUTPUT);
-  pinMode(motorRightOutput1, OUTPUT);
-  pinMode(motorRightOutput2, OUTPUT);
+  pinMode(motorRightInput1, OUTPUT);
+  pinMode(motorRightInput2, OUTPUT);
 
   pinMode(crashLeftPin, INPUT);
   pinMode(crashMiddlePin, INPUT);
   pinMode(crashRightPin, INPUT);
+
+  irLeft.enableIRIn();
+  irRight.enableIRIn();
 }
 
 void loop() {
-  //In phase #1, the OSV tries to escape the landing zone and traverse the rough terrain.
+  //In phase 1, the OSV tries to escape the landing zone and traverse the rough terrain.
   if(phase == 1){
     while(enes.location.theta > PI / 16 && enes.location.theta < 31 * PI / 16) {
       analogWrite(motorLeftEnable, 200);
@@ -80,7 +91,7 @@ void loop() {
     }
 
     if(enes.location.x > 1.1) {
-      enes.println("The OSV crossed the rough terrarain and entered phase #2.");
+      enes.println("The OSV crossed the rough terrarain and entered phase 2.");
       phase = 2;
     }
 
@@ -89,9 +100,9 @@ void loop() {
     }
   }
 
-  //In phase #2, the OSV searches for and moves towards the Black Box. If the OSV hits an obstacle, it enters phase #3. 
+  //In phase 2, the OSV searches for and moves towards the Black Box. If the OSV hits an obstacle, it enters phase #3. 
   while(phase == 2) {
-    while(infraredLeft == HIGH && infraredRight == LOW) {
+    while(irLeft.decode(&resultsLeft) && !irRight.decode(&resultsRight)) {
       enes.println("The OSV turns left.");
       digitalWrite(motorLeftInput1, HIGH);
       digitalWrite(motorLeftInput2, LOW);
@@ -100,7 +111,7 @@ void loop() {
       delay(checkTime);
     }
 
-    while(infraredLeft == LOW && infraredLeft == HIGH) {
+    while(!irLeft.decode(&resultsLeft) && irRight.decode(&resultsRight)) {
       enes.println("The OSV turns right.");
       digitalWrite(motorLeftInput1, LOW);
       digitalWrite(motorLeftInput2, HIGH);
@@ -115,8 +126,8 @@ void loop() {
     digitalWrite(motorRightInput1, LOW);
     digitalWrite(motorRightInput2, HIGH);
 
-    if(digitalRead(crashLeftPin) == HIGH || digitalRead(crashMiddlePin) == HIGH || digitalRead(crashRightPin) == HIGH) {
-      enes.println("The OSV hit something and entered phase #3.");
+    if(!digitalRead(crashLeftPin) == HIGH || !digitalRead(crashMiddlePin) == HIGH || !digitalRead(crashRightPin) == HIGH) {
+      enes.println("The OSV hit something and entered phase 3.");
       phase = 3;
     }
 
@@ -125,20 +136,20 @@ void loop() {
     }
   }
 
-  //In phase #3, the OSV decides what to when it hits something. It then follows a preset list of instructions to go around an obstacle.
+  //In phase 3, the OSV decides what to when it hits something. It then follows a preset list of instructions to go around an obstacle.
   while(phase == 3) {
-    \\This is where we decide what to do when we hit something
+    //This is where we decide what to do when we hit something
     if(!enes.updateLocation()) {
       enes.println("The OSV can't find its location.");
     }
 
     obstacleX = enes.location.x;
-    obstalceY = enes.location.y;
-    obstalceTheta = enes.location.theta;
+    obstacleY = enes.location.y;
+    obstacleTheta = enes.location.theta;
     
-    if(digitalRead(crashLeftPin) == HIGH && digitalRead(crashRightPin) == LOW) {
-      if(digitalRead(crashMiddlePin) == HIGH){
-        enes.print("The OSV will circumnavigate the obstacle on the right side.");
+    if(!digitalRead(crashLeftPin) == HIGH && !digitalRead(crashRightPin) == LOW) {
+      if(!digitalRead(crashMiddlePin) == HIGH){
+        enes.println("The OSV will circumnavigate the obstacle on the right side.");
         while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.y - obstacleY, 2)) < 0.30) {
           enes.println("The OSV moves backwards.");
           digitalWrite(motorLeftInput1, HIGH);
@@ -170,7 +181,7 @@ void loop() {
         obstacleX = enes.location.x;
         obstacleY = enes.location.y;
 
-        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2))) < 0.60) {
+        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2)) < 0.60) {
           enes.println("The OSV moves foward.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -184,7 +195,7 @@ void loop() {
           }
         }
 
-        while(infraredLeft == LOW && infraredRight == LOW) {
+        while(!irLeft.decode(&resultsLeft) && !irRight.decode(&resultsRight)) {
           enes.println("The OSV turns left.");
           digitalWrite(motorLeftInput1, HIGH);
           digitalWrite(motorLeftInput2, LOW);
@@ -198,8 +209,8 @@ void loop() {
           }
         }
       }
-      if(digitalRead(crashMiddlePin) == LOW){
-        enes.print("The OSV will circumnavigate the obstacle on the right side.");
+      if(!digitalRead(crashMiddlePin) == LOW){
+        enes.println("The OSV will circumnavigate the obstacle on the right side.");
         while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.y - obstacleY, 2)) < 0.20) {
           enes.println("The OSV moves backwards.");
           digitalWrite(motorLeftInput1, HIGH);
@@ -231,7 +242,7 @@ void loop() {
         obstacleX = enes.location.x;
         obstacleY = enes.location.y;
 
-        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2))) < 0.20) {
+        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2)) < 0.20) {
           enes.println("The OSV moves foward.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -245,7 +256,7 @@ void loop() {
           }
         }
 
-        while(infraredLeft == LOW && infraredRight == LOW) {
+        while(!irLeft.decode(&resultsLeft) && !irRight.decode(&resultsRight)) {
           enes.println("The OSV turns left.");
           digitalWrite(motorLeftInput1, HIGH);
           digitalWrite(motorLeftInput2, LOW);
@@ -261,9 +272,9 @@ void loop() {
       }
     }
     
-    if(digitalRead(crashLeftPin) == LOW && digitalRead(crashRightPin) == HIGH) {
-      if(digitalRead(crashMiddlePin) == HIGH) {
-        enes.print("The OSV will circumnavigate the obstacle on the left side.");
+    if(!digitalRead(crashLeftPin) == LOW && !digitalRead(crashRightPin) == HIGH) {
+      if(!digitalRead(crashMiddlePin) == HIGH) {
+        enes.println("The OSV will circumnavigate the obstacle on the left side.");
         while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.y - obstacleY, 2)) < 0.30) {
           enes.println("The OSV moves backwards.");
           digitalWrite(motorLeftInput1, HIGH);
@@ -295,7 +306,7 @@ void loop() {
         obstacleX = enes.location.x;
         obstacleY = enes.location.y;
 
-        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2))) < 0.60) {
+        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2)) < 0.60) {
           enes.println("The OSV moves foward.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -309,7 +320,7 @@ void loop() {
           }
         }
 
-        while(infraredLeft == LOW && infraredRight == LOW) {
+        while(!irLeft.decode(&resultsLeft) && !irRight.decode(&resultsRight)) {
           enes.println("The OSV turns right.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -324,8 +335,8 @@ void loop() {
         }
       }
       
-      if(digitalRead(crashMiddlePin) == LOW) {
-        enes.print("The OSV will circumnavigate the obstacle on the left side.");
+      if(!digitalRead(crashMiddlePin) == LOW) {
+        enes.println("The OSV will circumnavigate the obstacle on the left side.");
         while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.y - obstacleY, 2)) < 0.20) {
           enes.println("The OSV moves backwards.");
           digitalWrite(motorLeftInput1, HIGH);
@@ -357,7 +368,7 @@ void loop() {
         obstacleX = enes.location.x;
         obstacleY = enes.location.y;
 
-        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2))) < 0.20) {
+        while(sqrt(pow(enes.location.x - obstacleX, 2) + pow(enes.location.x - obstacleX, 2)) < 0.20) {
           enes.println("The OSV moves foward.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -371,7 +382,7 @@ void loop() {
           }
         }
 
-        while(infraredLeft == LOW && infraredRight == LOW) {
+        while(!irLeft.decode(&resultsLeft) && !irRight.decode(&resultsRight)) {
           enes.println("The OSV turns right.");
           digitalWrite(motorLeftInput1, LOW);
           digitalWrite(motorLeftInput2, HIGH);
@@ -387,19 +398,20 @@ void loop() {
       }
     }
     
-    if(digitalRead(crashLeftPin) == HIGH && digitalRead(crashRightPin) == HIGH) {
+    if(!digitalRead(crashLeftPin) == HIGH && !digitalRead(crashRightPin) == HIGH) {
       //go to the center of the arena
     }
     
-    if(digitalRead(crashMiddlePin) == HIGH && digitalRead(crashLeftPin) == LOW && digitalRead(crashRightPin) == LOW) {
+    if(!digitalRead(crashMiddlePin) == HIGH && !digitalRead(crashLeftPin) == LOW && !digitalRead(crashRightPin) == LOW) {
       analogWrite(motorLeftEnable, 0);
       analogWrite(motorLeftEnable, 0);
       enes.println("The OSV has found the Black Box.");
-      enes.navigated():
-      enes.baseObjective(Coordinate(enes.location.x, enes.location.y));
+      enes.navigated();
+      Coordinate blackbox(enes.location.x, enes.location.y);
+      enes.baseObjective(blackbox);
     }
 
-    enes.println("The OSV has navigated the obstacle and re-entered phase #2.");
+    enes.println("The OSV has navigated the obstacle and re-entered phase 2.");
     phase = 2;
   }
 }
